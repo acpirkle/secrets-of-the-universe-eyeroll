420b544bbd6449da44cb3dd347e83795eb0ba0dd
Andrew Pirkle <andrewcpirkle@gmail.com>
2020-11-08 23:21:12 -0500

    modify script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const fetch = require('node-fetch');
+
 /**
  * @function getVerse
  * fetches a random bible verse from bible.org's free api
  * @param none
  * @returns {JSON} object containing a random bible verse
  */
-async function getVerse(){
-   const verse = await fetch('https://labs.bible.org/api/?passage=random&type=json');
-   return verse.json();
+async function getVerse() {
+  const verse = await fetch(
+    "https://labs.bible.org/api/?passage=random&type=json"
+  );
+  return verse.json();
 }
 
 /**
@@ -15,7 +19,27 @@ async function getVerse(){
  * @param none
  * @returns Integer
  */
-function getRandomInt(){
-    return Math.floor(Math.random() * 10);
+function getRandomInt() {
+  return Math.floor(Math.random() * 10);
 }
 
+/**
+ * @function createVerses
+ * build and array of random bible verses
+ * @param none
+ * @returns array
+ */
+async function createVerses() {
+  const arrayLength = getRandomInt();
+  let verseArray = [];
+
+  for (let index = 0; index < arrayLength; index++) {
+    const verse = await getVerse();
+    verseArray.push(verse[0].text);
+  }
+
+  return verseArray;
+};
+
+
+createVerses().then(data => console.log(data.join(" ")));
